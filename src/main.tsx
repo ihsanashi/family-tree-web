04c6394ab8f25b1791945c9110dadc137a8f8868
Ahmad Ihsan <ihsanashi@gmail.com>
2024-04-06 20:43:13 +0800

    feat: setup file based routing with tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen';
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('app')!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <Theme>
+        <RouterProvider router={router} />
+      </Theme>
+    </React.StrictMode>
+  );
+}
